a713adecbcc9d85ee8af7ab98918ce81a9e281b6
HungNguyen-dl <hungnguyen9xbmt@gmail.com>
2024-09-29 14:32:33 +0700

    Main pages content complele

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/header/Header";
+import LatestCollection from "../components/LatestCollection";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <LatestCollection />
     </>
   );
 };
