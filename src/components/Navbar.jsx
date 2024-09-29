fb9f65306a70e1863c832ce2b2942cdd6d8f4e73
HungNguyen-dl <hungnguyen9xbmt@gmail.com>
2024-09-29 14:35:27 +0700

    Home pages content complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { assets } from "../assets/assets";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import CartTab from "./cartTab/CartTab";
 
 const Navbar = (props) => {
@@ -9,7 +9,7 @@ const Navbar = (props) => {
 
   return (
     <div className="flex items-center justify-between font-medium header-page">
-      <img src="./images/3.jpg" className="w-32" alt="" />
+      <Link to={'/'}><img src="./images/3.jpg" className="w-32" alt="" /></Link>
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
         <NavLink to="/" className="flex flex-col items-center gap-1">
           <p>HOME</p>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Header from "../components/header/Header";
 import LatestCollection from "../components/LatestCollection";
+import BestSeller from "../components/BestSeller";
+import OutPolicy from "../components/OutPolicy";
+import SubscribeBox from "../components/SubscribeBox";
 
 const Home = () => {
   return (
-    <>
+    <div>
       <Header />
       <LatestCollection />
-    </>
+      <BestSeller />
+      <OutPolicy />
+      <SubscribeBox />
+    </div>
   );
 };
 
